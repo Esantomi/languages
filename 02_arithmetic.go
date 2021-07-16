ec0fe4e8ca8bb877482bef2ad77bb45b7d368978
Esantomi <pilinnasa@gmail.com>
2021-07-16 11:13:43 +0900

    The four fundamental arithmetic operations + % operation

---
+++ 02_arithmetic.go
@@ -0,0 +1,19 @@
+// 사칙 연산 //
+// https://www.acmicpc.net/problem/10869 //
+
+package main
+
+import "fmt"
+
+func main() {
+	var A int
+	var B int
+
+	fmt.Print("두 자연수 A, B를 입력하시오: ")
+	fmt.Scanf("%d %d", &A, &B)
+	fmt.Println(A + B)
+	fmt.Println(A - B)
+	fmt.Println(A * B)
+	fmt.Println(A / B)
+	fmt.Println(A % B)
+}
