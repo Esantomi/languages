45a39a1abc893c3483c18bbce87422d2cf884b32
Esantomi <pilinnasa@gmail.com>
2021-07-17 20:50:46 +0900

    switch expressions

---
+++ nomadGo/07_switch.go
@@ -0,0 +1,40 @@
+// nomadcoders 쉽고 빠른 Go 시작하기 //
+// 1.7 switch //
+
+package main
+
+import "fmt"
+
+// if, else if 난무 방지에 도움이 된다.
+
+// switch 변수 {}
+func canIDrink3(age int) bool {
+	switch age { // 변수 age에 대해 조건 비교
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
+// switch {}
+func canIDrink4(age int) bool {
+	switch { // swtich만 쓰고 case에서 조건 비교
+	case age < 18:
+		return false
+	case age == 18:
+		return true
+	case age > 50:
+		return false
+	}
+	return false
+}
+
+// main
+func main() {
+	fmt.Println(canIDrink3(18)) // true
+	fmt.Println(canIDrink3(16)) // false
+	fmt.Println(canIDrink4(18)) // true
+	fmt.Println(canIDrink4(16)) // false
+}
