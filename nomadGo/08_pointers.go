fa3913a957f49cee99883e5d71a9a098abb82eb8
Esantomi <pilinnasa@gmail.com>
2021-07-17 21:39:13 +0900

    in a progress

--- nomadGo/08_pointers.go
+++ nomadGo/08_pointers.go
@@ -7,10 +7,23 @@ import "fmt"
 
 // 메모리에 접근해 주소, 주소에 저장된 값 등 조회 가능
 // low-level programming을 가능하게 해 준다.
+// &: 메모리 주소
+// *: 여기서부터 공부
 
 // main
 func main() {
+	// 값 복사
 	a := 2
-	b := a
-	fmt.Println(a, b)
+	b := a // 이 시점에서 a가 2였으므로 뒤에 a 값을 바꿔도 b는 계속 2이다.
+	a = 10 // a 값 변경. b는 그대로 2이다.
+
+	// 주소 복사
+	aa := 2
+	bb := &aa // aa의 값 대신 주소 복사 후 bb에 할당
+	aa = 10
+
+	fmt.Println(a, b)    // 10 2
+	fmt.Println(&a, &b)  // 0xc000016118 0xc000016140 (메모리 주소)
+	fmt.Println(aa, bb)  // bb 주소는 aa 주소와 같다.
+	fmt.Println(&aa, bb) // 0xc000124020 0xc000124020 (주소가 동일함)
 }
