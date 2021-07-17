7ec3d4f9edcb78d03632b267eebb8a31b9a69c46
Esantomi <pilinnasa@gmail.com>
2021-07-17 21:18:35 +0900

    in a progress

--- nomadGo/08_pointers.go
+++ nomadGo/08_pointers.go
@@ -5,21 +5,12 @@ package main
 
 import "fmt"
 
-// low-level programming
-
-// switch 변수 {}
-func canIDrink3(age int) bool {
-	switch age { // 변수 age에 대해 조건 비교
-	case 10: // age가 10인 경우
-		return false
-	case 18: // age가 18인 경우
-		return true
-	}
-	return false // case에 해당 사항이 없는 경우
-}
+// 메모리에 접근해 주소, 주소에 저장된 값 등 조회 가능
+// low-level programming을 가능하게 해 준다.
 
 // main
 func main() {
-	fmt.Println(canIDrink3(18)) // true
-	fmt.Println(canIDrink3(16)) // false
+	a := 2
+	b := a
+	fmt.Println(a, b)
 }
