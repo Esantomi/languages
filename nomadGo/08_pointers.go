77484edd14673116d2f5ac022afe84e4126c702c
Esantomi <pilinnasa@gmail.com>
2021-07-18 16:18:52 +0900

    pointers

--- nomadGo/08_pointers.go
+++ nomadGo/08_pointers.go
@@ -7,8 +7,12 @@ import "fmt"
 
 // 메모리에 접근해 주소, 주소에 저장된 값 등 조회 가능
 // low-level programming을 가능하게 해 준다.
-// &: 변수의 메모리 주소
-// *: 여기서부터 공부
+
+// &: 포인터(pointer) 변수의 메모리 주소 전달
+// *: 디리퍼런싱(dereferencing). 주소에 값을 쓸 때 사용
+// *를 type 앞에 쓰면 parameter가 포인터임을 표시 (look through)
+// e.g. func say(msg *string) {}:
+// 이때의 msg는 string이 아닌 string을 갖는 메모리 주소
 
 // main
 func main() {
@@ -19,11 +23,15 @@ func main() {
 
 	// 주소 복사
 	aa := 2
-	bb := &aa // aa의 값 대신 주소 복사 후 bb에 할당
+	bb := &aa        // aa의 값 대신 주소 복사 후 bb에 할당
+	fmt.Println(*bb) // 2
 	aa = 10
+	fmt.Println(*bb) // 10 (aa = 10 재할당 때문)
+	// *bb는 bb의 값, bb의 값은 aa의 메모리 주소, 따라서 10
 
-	fmt.Println(a, b)    // 10 2
-	fmt.Println(&a, &b)  // 0xc000016118 0xc000016140 (메모리 주소 다름)
+	fmt.Println(a, b)   // 10 2
+	fmt.Println(&a, &b) // 0xc000016118 0xc000016140 (메모리 주소 다름)
+
 	fmt.Println(aa, bb)  // 10 0xc000016148
 	fmt.Println(&aa, bb) // 0xc000016148 0xc000016148 (주소가 동일함)
 }
