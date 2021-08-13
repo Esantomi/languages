703f65ca771ae6936ef6995f0b91773393c83a97
Esantomi <pilinnasa@gmail.com>
2021-08-13 19:40:58 +0900

    comparison operator of Haskell

---
+++ 02_truthValues.hs
@@ -0,0 +1,14 @@
+{- Wikibooks Haskell -}
+-- https://wikidocs.net/book/204
+
+
+{- 1.3 진위값 -}
+f x = x + 3          -- 함수 f 정의
+area r = pi * r ^ 2  -- 함수 area 정의
+
+{- 비교 연산자
+==(항등), <, >, <=, >= -}
+
+-- f 2 == 5     -- True
+-- f 3 == 5     -- False
+-- area 5 < 50  -- False
