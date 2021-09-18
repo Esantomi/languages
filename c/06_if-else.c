957a6f5630a2adefe750f4bc322c97cc1f8c5a61
Esantomi <pilinnasa@gmail.com>
2021-09-18 14:56:08 +0900

    blocking division by 0 with if statement

--- c/06_if-else.c
+++ c/06_if-else.c
@@ -4,13 +4,27 @@
 
 #include <stdio.h>
 int main() {
-    int i;
+    /* if 문 */
+    int a;
     printf("아무 숫자나 입력하세요! : ");
-    scanf("%d", &i);
+    scanf("%d", &a);
 
-    if (i == 7) {
+    if (a == 7) {
         printf("행운의 숫자 7을 입력했네요!");
     }
+
+
+    /* 나눗셈 예제 */
+    double i, j
+    printf("나눌 두 정수를 입력하세요! : ");
+    scanf("%lf %lf", &i, &j);
+
+    if (j == 0) {
+        printf("0으로 나누면 inf가 출력됩니다. \n");
+        return 0;
+    }
+
+    printf("%f를 %f로 나눈 값은 : %f \n", i, j, i / j);
 
     return 0;
 }
