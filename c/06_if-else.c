70dec9507835862e968db704ea471ae45d50317e
Esantomi <pilinnasa@gmail.com>
2021-09-18 13:53:56 +0900

    if statement of C

---
+++ c/06_if-else.c
@@ -0,0 +1,16 @@
+// 씹어먹는 C 언어 (modoocode.com)
+// 6. 만약에...(if 문)
+
+
+#include <stdio.h>
+int main() {
+    int i;
+    printf("아무 숫자나 입력하세요! : ");
+    scanf("%d", &i);
+
+    if (i == 7) {
+        printf("행운의 숫자 7을 입력했네요!");
+    }
+
+    return 0;
+}
