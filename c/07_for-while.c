79b95b2a3abe7fdf8dd586b3d8490c9c7d959120
Esantomi <pilinnasa@gmail.com>
2021-09-19 14:03:28 +0900

    summation of 1 to 19

--- c/07_for-while.c
+++ c/07_for-while.c
@@ -2,6 +2,8 @@
 // 6. 만약에...(if 문)
 
 
+// for 문 : {}를 조건식이 성립하는 동안 반복
+
 #include <stdio.h>
 int main() {
     /* 숫자 0부터 19까지 출력 */
@@ -9,6 +11,13 @@ int main() {
     for (i = 0; i < 20; i++) {
         printf("숫자 : %d \n", i);
     }
+
+    /* 1부터 19까지의 합 */
+    int sum = 0;
+    for (i = 0; i < 20; ++i) {
+        sum += i;
+    }
+    printf("1부터 19까지의 합 : %d \n", sum);  // 190
 
     return 0;
 }
