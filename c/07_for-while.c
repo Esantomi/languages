e5ad879e50c3ea1462154d98abd9b3c4a0c84475
Esantomi <pilinnasa@gmail.com>
2021-09-19 13:47:20 +0900

    for statement of C language

---
+++ c/07_for-while.c
@@ -0,0 +1,14 @@
+// 씹어먹는 C 언어 (modoocode.com)
+// 6. 만약에...(if 문)
+
+
+#include <stdio.h>
+int main() {
+    /* 숫자 0부터 19까지 출력 */
+    int i;
+    for (i = 0; i < 20; i++) {
+        printf("숫자 : %d \n", i);
+    }
+
+    return 0;
+}
