8e945fdf1d911029937c629b5485457b23c1dc4c
Esantomi <pilinnasa@gmail.com>
2021-09-24 21:30:55 +0900

    differences of inner logics between if-else statement and switch sentence

--- c/08_switch.c
+++ c/08_switch.c
@@ -2,6 +2,15 @@
 // 8. 만약에... 2탄 (switch 문)
 
 
+/* if-else와 switch 내부 로직 차이 */
+
+// if ... else if ... else if ... else는 총 3번 비교한다. (else는 비교 없는 자동 처리)
+// 즉 if 문은 CMP 연산(어셈블리 compare)을 모든 case에 대해 시행할 수 있다.
+
+// switch는 jump table을 생성하는데, table의 각 값은 case 별 명령어 주소를 가리킨다.
+// 따라서 switch를 쓰면 case에 따라 CMP 연산이 느는 게 아닌, jump table의 크기만 커지고 성능에는 영향이 없다.
+
+
 #include <stdio.h>
 int main() {
     /* 귀여운 꼬깜이 */
@@ -36,7 +45,7 @@ int main() {
 
 
     /* 알파벳을 읽어요 */
-    char input;
+    char input;  // 컴퓨터는 문자를 모두 숫자로 처리, 보여 줄 때만 문자로 보여 주므로 '문자 = 정수'라 봐도 무방함
 
     printf("알파벳 소문자 읽기 \n");
     printf("알파벳 : ");
