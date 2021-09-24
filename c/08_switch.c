85130e628943202f13f8d402460352384b9d7add
Esantomi <pilinnasa@gmail.com>
2021-09-24 21:23:06 +0900

    switch statement with character data inputs

--- c/08_switch.c
+++ c/08_switch.c
@@ -34,5 +34,33 @@ int main() {
         break;
     }
 
+
+    /* 알파벳을 읽어요 */
+    char input;
+
+    printf("알파벳 소문자 읽기 \n");
+    printf("알파벳 : ");
+
+    scanf("%c", &input);
+
+    switch (input)
+    {
+    case 'a':
+        printf("에이 \n");
+        break;
+    
+    case 'b':
+        printf("비 \n");
+        break;
+
+    case 'c':
+        printf("씨 \n");
+        break;
+
+    default:
+        printf("C 밑으로 받으면 곤란합니다. \n");
+        break;
+    }
+
     return 0;
 }
